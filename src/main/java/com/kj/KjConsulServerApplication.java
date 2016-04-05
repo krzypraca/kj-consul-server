ecfdac2908703d428508bddffdea6e67a2fd026f
kj <krzypraca@gmx.com>
2016-04-05 14:48:49 -0700

    First commit

---
+++ src/main/java/com/kj/KjConsulServerApplication.java
@@ -0,0 +1,20 @@
+package com.kj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class KjConsulServerApplication {
+	@RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+	public static void main(String[] args) {
+		SpringApplication.run(KjConsulServerApplication.class, args);
+	}
+}
